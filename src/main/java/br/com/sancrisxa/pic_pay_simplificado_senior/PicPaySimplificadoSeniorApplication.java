ff64a8a190411563f8b0262b355615006baa2031
Samuel Xavier <sancrisxa@gmail.com>
2025-01-12 17:02:48 -0300

    first commit

---
+++ src/main/java/br/com/sancrisxa/pic_pay_simplificado_senior/PicPaySimplificadoSeniorApplication.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.pic_pay_simplificado_senior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplificadoSeniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplificadoSeniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sancrisxa/pic_pay_simplificado_senior/PicPaySimplificadoSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.pic_pay_simplificado_senior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySimplificadoSeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
